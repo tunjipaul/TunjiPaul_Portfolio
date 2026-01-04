af3bc7a0c258be86bdbec7b5ebf03cc317219b6f
TunjiPaul <crashing45@gmail.com>
2026-01-04 08:50:10 +0100

    Update ProtectedRoute.jsx

--- TunjiPaul-Portfolio/src/pages/ProtectedRoute.jsx
+++ TunjiPaul-Portfolio/src/pages/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
-  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
 
   if (!isLoggedIn) {
     return <Navigate to="/login" replace />;
